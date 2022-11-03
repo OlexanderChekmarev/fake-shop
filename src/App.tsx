068e6388f95d81a95260c299394685b85ca4423d
Olexander <alexanderchekmarev1987@gmail.com>
2022-11-03 19:40:00 +0200

    wright function react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
 
-const title = <h1>Hello React.js</h1>
-
-const list = (
-    <ul>
-    <li>list item 1</li>
-    <li>list item 2</li>
-    <li>list item 3</li>
-    </ul>
-)
+// React Element
+// const title = <h1>Hello React.js</h1>
 
 
+// React component
+const Title = () => <h1>Hello React component</h1>
+
+const List = () => {
+    let a = 10
+    return (
+      <ul>
+           <li>list item {a + 1}</li>
+           <li>list item 2</li>
+           <li>list item 3</li>
+      </ul>
+   )
+}
 
 function App() {
     return (
     <div className="App">
-        {title} {list} {10 + 5}
+        <Title />
+        <List />
+        {10 + 5}
     </div>
     )
 }
-
 export default App
