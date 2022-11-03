3d126e40ce477afaf1fc3b728c9244169c8c7ece
Olexander <alexanderchekmarev1987@gmail.com>
2022-11-03 19:55:40 +0200

    add React.Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,40 @@ import React from 'react'
 // React Element
 // const title = <h1>Hello React.js</h1>
 
-
 // React component
 const Title = () => <h1>Hello React component</h1>
 
 const List = () => {
     let a = 10
     return (
-      <ul>
-           <li>list item {a + 1}</li>
-           <li>list item 2</li>
-           <li>list item 3</li>
-      </ul>
-   )
+      <React.Fragment>
+            <ul>
+                <li>list item {a + 1}</li>
+                <li>list item 2</li>
+                <li>list item 3</li>
+            </ul>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque,
+                cum quidem ad provident veritatis unde, placeat dolor sequi vel
+                minus tenetur doloribus assumenda, impedit ut sapiente?
+                Possimus, exercitationem! Modi, asperiores.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ab
+                aspernatur rem repellendus cupiditate fuga, deserunt libero esse
+                omnis odio et corporis. Rerum veniam doloremque mollitia iusto
+                ratione adipisci nulla.
+            </p>
+     </React.Fragment>
+    )
 }
 
 function App() {
     return (
-    <div className="App">
-        <Title />
-        <List />
-        {10 + 5}
-    </div>
+        <div className="App">
+            <Title />
+            <List />
+        </div>
     )
 }
 export default App
