847ac2a6dbf5e2d70c0860c79a853f24e20d2f9e
Olexander <alexanderchekmarev1987@gmail.com>
2022-11-13 14:42:42 +0200

    add ProductListItem

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
+import ProductListItem from './ProductListItem'
 
 type Props = {}
 
@@ -14,8 +15,19 @@ const ProductsList = (props: Props) => {
                     textTransform: 'uppercase',
                 }}
             >
-                ProductList
+                ProductsList
             </Typography>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={4}
+            >
+                <Grid item>
+                    <ProductListItem />
+                </Grid>
+            </Grid>
         </>
     )
 }
